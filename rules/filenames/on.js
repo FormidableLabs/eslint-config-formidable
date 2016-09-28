40f86b94b1eb024a0f9bacd9eb8e0af935e5bded
Ryan Roemer <ryan@loose-bits.com>
2016-09-28 10:04:40 -0700

    Update filenames/match-regex to always enforce.

--- rules/filenames/on.js
+++ rules/filenames/on.js
@@ -6,8 +6,10 @@ module.exports = {
   ],
   "rules": {
     // Enforce dash-cased filenames
-    "filenames/match-regex": ["error", "^[a-z0-9\\-\\.]+$", true],
+    // `true` here means "don't enforce if `export` exists in file"
+    "filenames/match-regex": ["error", "^[a-z0-9\\-\\.]+$", false],
     // Match the file name against the default exported value in the module
+    // Should set to `"error"` if last parameter above is `true`.
     "filenames/match-exported": "off",
     // Don't allow index.js files
     "filenames/no-index": "off"
